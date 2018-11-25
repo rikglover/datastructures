f91e0ea20e272e5a75c5c4a7e6811f68c6daf16a
rikglover <rikglover@gmail.com>
2018-11-25 13:58:01 -0800

    Chapter 1, Section 1 (initial commit)

---
+++ chapter1/src/main/java/numericfunctions/NumericFunctions.java
@@ -0,0 +1,23 @@
+package numericfunctions;
+
+public class NumericFunctions {
+
+  private static int factorial(int n) {
+    int result = 1;
+
+    for (int i = 2; i <= n; i++) {
+      result *= i;
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    for (int n = 1; n <= 10; n++) {
+      System.out.print(n);
+      System.out.print(" ");
+      System.out.println(factorial(n));
+    }
+  }
+}
+
