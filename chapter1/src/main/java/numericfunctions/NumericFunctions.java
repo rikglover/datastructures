3b589a8631643a5c95c741f557ecd40a7c85f796
rikglover <rikglover@gmail.com>
2018-11-25 20:56:40 -0800

    Chapter 1, Section 1 (Through question 7)

--- chapter1/src/main/java/numericfunctions/NumericFunctions.java
+++ chapter1/src/main/java/numericfunctions/NumericFunctions.java
@@ -12,11 +12,35 @@ public class NumericFunctions {
     return result;
   }
 
+  private static long pow(long m, long n) {
+    if(n < 0) {
+      throw new IllegalArgumentException("N must be greater than or equal to zero");
+    }
+
+    long result = 1;
+
+    for(long i = 0; i < n; i++) {
+      result *= m;
+    }
+
+    return result;
+  }
+
   public static void main(String[] args) {
     for (int n = 1; n <= 10; n++) {
       System.out.print(n);
       System.out.print(" ");
       System.out.println(factorial(n));
+    }
+
+    System.out.println("\n\n");
+
+    for (int i = 1; i <= 9; i++) {
+      for(int j = 1; j <= 9; j++) {
+        System.out.print(pow(i, j) + "  ");
+      }
+
+      System.out.println();
     }
   }
 }
