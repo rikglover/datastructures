a33614d2c0b50abe53d819426e175578f7ba3e41
rikglover <rikglover@gmail.com>
2018-12-27 22:17:44 -0800

    Chapter 5 (Through question 6)

--- chapter5/src/main/java/queue/LinkedQueue.java
+++ chapter5/src/main/java/queue/LinkedQueue.java
@@ -67,6 +67,56 @@ public class LinkedQueue<E> implements Queue<E> {
     return size;
   }
 
+  public static void main(String[] args) {
+    Queue<Integer> integerQueue = new LinkedQueue<>();
+
+    integerQueue.enqueue(5);
+    integerQueue.enqueue(6);
+    integerQueue.enqueue(7);
+
+    System.out.println("\n\nFront of queue: " + integerQueue.peek());
+    System.out.println("Size of queue: " + integerQueue.size());
+    System.out.println();
+
+    while(!integerQueue.isEmpty()) {
+      System.out.println("Dequeing " + integerQueue.dequeue());
+    }
+
+    System.out.println("\n");
+
+    Queue<Double> doubleQueue = new LinkedQueue<>();
+
+    doubleQueue.enqueue(5.0);
+    doubleQueue.enqueue(6.0);
+    doubleQueue.enqueue(7.0);
+
+    System.out.println("Front of queue: " + doubleQueue.peek());
+    System.out.println("Size of queue: " + doubleQueue.size());
+    System.out.println();
+
+    while(!doubleQueue.isEmpty()) {
+      System.out.println("Dequeing " + doubleQueue.dequeue());
+    }
+
+    System.out.println("\n");
+
+    Queue<String> stringQueue = new LinkedQueue<>();
+
+    stringQueue.enqueue("Five");
+    stringQueue.enqueue("Six");
+    stringQueue.enqueue("Seven");
+
+    System.out.println("Front of queue: " + stringQueue.peek());
+    System.out.println("Size of queue: " + stringQueue.size());
+    System.out.println();
+
+    while(!stringQueue.isEmpty()) {
+      System.out.println("Dequeing " + stringQueue.dequeue());
+    }
+
+    System.out.println("\n");
+  }
+
   @Getter
   @Setter
   @AllArgsConstructor
