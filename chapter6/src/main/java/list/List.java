eaedcd081f2f201f9d84c801a7dc526f1ed11ce8
rikglover <rikglover@gmail.com>
2019-01-02 22:56:34 -0800

    Chapter 6 (Answers for Section 1 completed)

---
+++ chapter6/src/main/java/list/List.java
@@ -0,0 +1,20 @@
+package list;
+
+public interface List<E> {
+
+  void add(E item);
+
+  void add(int index, E item);
+
+  E get(int index);
+
+  int indexOf(E item);
+
+  boolean isEmpty();
+
+  E removeAt(int index);
+
+  E set(int index, E item);
+
+  int size();
+}
