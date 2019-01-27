768a39071bbc2e73fd43f5477bd2517ce24f9f33
rikglover <rikglover@gmail.com>
2019-01-27 12:03:32 -0800

    Chapter 7 (After checking answers)

--- chapter7/src/main/java/Recursion.java
+++ chapter7/src/main/java/Recursion.java
@@ -52,8 +52,8 @@ public class Recursion {
   }
 
   public static int fib(int n) {
-    if (n == 0 || n == 1) {
-      return 1;
+    if (n <= 1) {
+      return n;
     } else {
       return fib(n - 1) + fib(n - 2);
     }
